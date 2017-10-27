de8ff83d1455c208d7bb80cee80fc95f49b4b0b1
munspel <munspel@10.1.10.24>
2017-10-27 13:15:49 +0300

    Fix annotation warning

---
+++ WebLib/HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+namespace WebLib;
+
+class HelloWorld {
+    
+    public function __construct() {
+        echo "Hello! It's Lib\WebLib\HelloWorld";
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
